38f7c96fa7345bd900d294b547b5247c4aaee27a
Thomas Ingram <code@ravinggenius.com>
2020-11-05 20:41:59 -0500

    Update dependencies

--- src/components/layouts/Primary.js
+++ src/components/layouts/Primary.js
@@ -1,90 +1,88 @@
 import { Global, css } from '@emotion/core';
-import { StaticQuery, graphql } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import PropTypes from 'prop-types';
 import exact from 'prop-types-exact';
 import React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 
 import '@csstools/normalize.css';
 
-const Primary = ({ children }) => (
-	<StaticQuery
-		query={graphql`
-			query SiteMetaQuery {
-				contact: markdownRemark(fields: { slug: { eq: "/contact" } }) {
-					frontmatter {
-						name
-					}
+const Primary = ({ children }) => {
+	const { contact } = useStaticQuery(graphql`
+		query SiteMetaQuery {
+			contact: markdownRemark(fields: { slug: { eq: "/contact" } }) {
+				frontmatter {
+					name
 				}
 			}
-		`}
-	>
-		{data => (
-			<>
-				<Helmet>
-					<meta charSet="utf-8" />
+		}
+	`);
 
-					<title>{`Résumé | ${data.contact.frontmatter.name}`}</title>
+	return (
+		<>
+			<Helmet>
+				<meta charSet="utf-8" />
 
-					<meta
-						name="description"
-						content={`Résumé of ${data.contact.frontmatter.name}`}
-					/>
-				</Helmet>
+				<title>{`Résumé | ${contact.frontmatter.name}`}</title>
 
-				<Global
-					styles={css`
-						body {
-							color: #333333;
-							font-family: 'Lato', 'Helvetica Neue', Helvetica,
-								Arial, sans-serif;
-							margin-left: auto;
-							margin-right: auto;
-							max-width: 8in;
-
-							@media print {
-								max-width: 7in;
-							}
-						}
-
-						a {
-							color: #337ab7;
-							text-decoration: none;
-
-							&:focus,
-							&:hover {
-								color: #23527c;
-								text-decoration: underline;
-							}
-						}
-
-						h1,
-						h2,
-						h3,
-						h4,
-						h5,
-						h6 {
-							font-weight: 500;
-							margin: 0;
-						}
-
-						@media screen {
-							main {
-								margin: 1em;
-							}
-						}
-
-						p {
-							margin: 0;
-						}
-					`}
+				<meta
+					name="description"
+					content={`Résumé of ${contact.frontmatter.name}`}
 				/>
+			</Helmet>
 
-				<main role="main">{children}</main>
-			</>
-		)}
-	</StaticQuery>
-);
+			<Global
+				styles={css`
+					body {
+						color: #333333;
+						font-family: 'Lato', 'Helvetica Neue', Helvetica, Arial,
+							sans-serif;
+						margin-left: auto;
+						margin-right: auto;
+						max-width: 8in;
+
+						@media print {
+							max-width: 7in;
+						}
+					}
+
+					a {
+						color: #337ab7;
+						text-decoration: none;
+
+						&:focus,
+						&:hover {
+							color: #23527c;
+							text-decoration: underline;
+						}
+					}
+
+					h1,
+					h2,
+					h3,
+					h4,
+					h5,
+					h6 {
+						font-weight: 500;
+						margin: 0;
+					}
+
+					@media screen {
+						main {
+							margin: 1em;
+						}
+					}
+
+					p {
+						margin: 0;
+					}
+				`}
+			/>
+
+			<main role="main">{children}</main>
+		</>
+	);
+};
 
 Primary.propTypes = exact({
 	children: PropTypes.node.isRequired
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
-import { StaticQuery, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 import React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet';
 
 import ContactCard from '../components/ContactCard';
 import CompanyCard from '../components/CompanyCard';
@@ -17,115 +17,111 @@ import { calculatePropminenceFor, weighByExperience } from '../utilities';
 
 const MIN_PROMINENCE_THRESHOLD = 3;
 
-const IndexPage = () => (
-	<StaticQuery
-		query={graphql`
-			query IndexQuery {
-				companies: allMarkdownRemark(
-					filter: { frontmatter: { isPublished: { eq: true } } }
-					sort: { fields: [frontmatter___startedAt], order: DESC }
-				) {
-					edges {
-						node {
-							fields {
-								slug
-							}
-							frontmatter {
-								keywords
-								name
-								startedAt
-								stoppedAt
-								title
-							}
-							summary: html
-						}
-					}
-				}
+const IndexPage = ({ data: { companies, contact } }) => {
+	const keywords = weighByExperience(
+		companies.edges.map(({ node }) => ({
+			keywords: node.frontmatter.keywords,
+			startedAt: node.frontmatter.startedAt,
+			stoppedAt: node.frontmatter.stoppedAt
+		}))
+	);
 
-				contact: markdownRemark(fields: { slug: { eq: "/contact" } }) {
-					frontmatter {
-						name
-						email
-						location
-						phone
-						title
-						website
-					}
-					profile: html
-				}
-			}
-		`}
-	>
-		{data => {
-			const keywords = weighByExperience(
-				data.companies.edges.map(({ node }) => ({
-					keywords: node.frontmatter.keywords,
-					startedAt: node.frontmatter.startedAt,
-					stoppedAt: node.frontmatter.stoppedAt
-				}))
-			);
+	const prominenceFor = calculatePropminenceFor(keywords);
 
-			const prominenceFor = calculatePropminenceFor(keywords);
+	const keywordsWithProminence = keywords.map(
+		({ lastUsed, weight, ...keyword }) => ({
+			lastUsed,
+			weight,
+			prominence: prominenceFor(lastUsed, weight),
+			...keyword
+		})
+	);
 
-			const keywordsWithProminence = keywords.map(
-				({ lastUsed, weight, ...keyword }) => ({
-					lastUsed,
-					weight,
-					prominence: prominenceFor(lastUsed, weight),
-					...keyword
-				})
-			);
+	const mostProminentKeywords = keywordsWithProminence.filter(
+		({ prominence }) => prominence >= MIN_PROMINENCE_THRESHOLD
+	);
 
-			const mostProminentKeywords = keywordsWithProminence.filter(
-				({ prominence }) => prominence >= MIN_PROMINENCE_THRESHOLD
-			);
+	return (
+		<Primary>
+			<Helmet>
+				<meta
+					name="keywords"
+					content={mostProminentKeywords
+						.map(({ keyword }) => keyword)
+						.join(', ')}
+				/>
+			</Helmet>
 
-			return (
-				<Primary>
-					<Helmet>
-						<meta
-							name="keywords"
-							content={mostProminentKeywords
-								.map(({ keyword }) => keyword)
-								.join(', ')}
-						/>
-					</Helmet>
+			<ContactCard {...contact.frontmatter} />
 
-					<ContactCard {...data.contact.frontmatter} />
+			<SectionLayout>
+				<Header>
+					<Title>Profile</Title>
+				</Header>
 
-					<SectionLayout>
-						<Header>
-							<Title>Profile</Title>
-						</Header>
+				<Body
+					css={markdownExtra}
+					dangerouslySetInnerHTML={{
+						__html: contact.profile
+					}}
+				/>
+			</SectionLayout>
 
-						<Body
-							css={markdownExtra}
-							dangerouslySetInnerHTML={{
-								__html: data.contact.profile
-							}}
-						/>
-					</SectionLayout>
+			<Section title="Skills/Tools">
+				<WeightedKeywordList {...{ keywords }} />
+			</Section>
 
-					<Section title="Skills/Tools">
-						<WeightedKeywordList {...{ keywords }} />
-					</Section>
-
-					<Section title="Experience">
-						{data.companies.edges.map(({ node }) => (
-							<CompanyCard
-								key={node.fields.slug}
-								name={node.frontmatter.name}
-								startedAt={node.frontmatter.startedAt}
-								stoppedAt={node.frontmatter.stoppedAt}
-								summary={node.summary}
-								title={node.frontmatter.title}
-							/>
-						))}
-					</Section>
-				</Primary>
-			);
-		}}
-	</StaticQuery>
-);
+			<Section title="Experience">
+				{companies.edges.map(({ node }) => (
+					<CompanyCard
+						key={node.fields.slug}
+						name={node.frontmatter.name}
+						startedAt={node.frontmatter.startedAt}
+						stoppedAt={node.frontmatter.stoppedAt}
+						summary={node.summary}
+						title={node.frontmatter.title}
+					/>
+				))}
+			</Section>
+		</Primary>
+	);
+};
 
 export default IndexPage;
+
+export const query = graphql`
+	query IndexQuery {
+		companies: allMarkdownRemark(
+			filter: { frontmatter: { isPublished: { eq: true } } }
+			sort: { fields: [frontmatter___startedAt], order: DESC }
+		) {
+			edges {
+				node {
+					fields {
+						slug
+					}
+					frontmatter {
+						keywords
+						name
+						startedAt
+						stoppedAt
+						title
+					}
+					summary: html
+				}
+			}
+		}
+
+		contact: markdownRemark(fields: { slug: { eq: "/contact" } }) {
+			frontmatter {
+				name
+				email
+				location
+				phone
+				title
+				website
+			}
+			profile: html
+		}
+	}
+`;
